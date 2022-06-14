8a40787b54ad55164b7a8895bfc26e8a72069631
adolenec <andrija.dolenec23@gmail.com>
2022-06-14 17:37:50 +0200

    Format code

--- src/helpers/api/api-drivers.js
+++ src/helpers/api/api-drivers.js
@@ -2,16 +2,15 @@ import { getCurrentYear } from "../helper-variables";
 
 const currentYear = getCurrentYear();
 
-export async function getCurrentDrivers () {
-    const response = await fetch(`http://ergast.com/api/f1/${currentYear}/drivers.json`);
-    const data = await response.json();
+export async function getCurrentDrivers() {
+  const response = await fetch(
+    `http://ergast.com/api/f1/${currentYear}/drivers.json`
+  );
+  const data = await response.json();
 
+  const drivers = data.MRData.DriverTable.Drivers;
 
-    const drivers = data.MRData.DriverTable.Drivers;
+  console.log(drivers);
 
-    // drivers.push(data.)
-
-    console.log(drivers);
-
-    return drivers;
+  return drivers;
 }

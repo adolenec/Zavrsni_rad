3d12bf01fb4c5f8ccbdd72fbf55593cc8430d17a
adolenec <andrija.dolenec23@gmail.com>
2022-06-21 08:59:47 +0200

    Add Route and NavLink components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import StartingPage from "./pages/StartingPage";
+import { Route, Switch, Redirect } from "react-router-dom";
+import MainNavigation from "./components/header/MainNavigation";
 
 function App() {
   return (
-    <>
-      <StartingPage />
-    </>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/starting-page" />
+      </Route>
+      <Route path="/starting-page">
+        <StartingPage />
+      </Route>
+      <Route path="/drivers">
+        <MainNavigation/>
+      </Route>
+      <Route path="/constructors"></Route>
+      <Route path="/circuits"></Route>
+      <Route path="/standings"></Route>
+      <Route path="/schedule"></Route>
+      <Route path="/history"></Route>
+    </Switch>
   );
 }
 
